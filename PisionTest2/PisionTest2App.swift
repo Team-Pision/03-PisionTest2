e0d075b95301e3cf609a9e357751408261d53420
YeoSeongil <seongil_yeo@naver.com>
2025-07-13 14:40:45 +0900

    git init

---
+++ PisionTest2/PisionTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  PisionTest2App.swift
+//  PisionTest2
+//
+//  Created by 여성일 on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PisionTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
